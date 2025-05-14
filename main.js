ba7904480a9094a3a9605f71b2d1d6550a4d04fc
sevenseaskynet <ashiyanaapna2@gmail.com>
2025-05-14 18:07:05 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,                  // Infinite loop
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
+  
